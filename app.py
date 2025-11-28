5079dafcd4e3193034fb5f196d716f08fa75dcc1
jamesonthe5th <24nathan.carter@gmail.com>
2025-11-27 21:57:28 -0500

    Enhance mergesort with step tracking
    Modified mergesort and merge functions to include step tracking for debugging. Added steps parameter to log the sorting process.

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
-def mergesort(a):
+def mergesort(a,steps):
   """ Takes unsorted list and sorts using merge sort and merge"""
+  steps.append("Merge sort initiated on {a}")
+  
   if len(a) < = 1:
+    steps.append("Reached length of 1 or 0 ({a}); list is already sorted; returning unchanged list")
     return a #already sorted array
 
   mid = len(a) //2
   left = a[:mid] 
-  right = a[mid:] #split into two halves
+  right = a[mid:]#split into two halves
+  steps.append("Dividing into two halfs \n Left half: {left}\n Right half:{right}")
 
   #recursive sorting part:
-  left_sort = mergesort(left)
-  right_sort = mergesort(right)
+  left_sort = mergesort(left,steps)
+  right_sort = mergesort(right,steps)
 
-  return merge(left_sort,right_sort) #call other function to return merged list
+  return merge(left_sort,right_sort,steps) #call other function to return merged list
 
-def merge(left,right):
+def merge(left,right, steps):
+  steps.append{"Merging the sorted {left} and {right} arrays")
   result = [] #initialize list
   i = j = 0 #set equal to 0
 
   while i < len(left) and j < len(right): #compare elements and append the smaller
     if left[i] < right[j]:
       result.append(left[i])
+      steps.append("")
       i +=1 #increment i by 1
     else:
       result.append(right[j])
+      steps.append("")
       j +=1 #increment j by one
 
-  result.extend(left[i:]) #appends leftover elements
-  result.extend(right[j:]) #ai disclaimer needed help with this part didn't know how to append the leftover elements
+
+
+def merge_sort_steps_display(
 
 
