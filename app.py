26cccaf330d6b56f085c973c3acee640a450ef7f
jamesonthe5th <24nathan.carter@gmail.com>
2025-11-27 22:15:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,28 +12,44 @@ def mergesort(a,steps):
   steps.append("Dividing into two halfs \n Left half: {left}\n Right half:{right}")
 
   #recursive sorting part:
+  steps.append("Recursively sorting the left half: {left}")
   left_sort = mergesort(left,steps)
+  
+  steps.append("Recursively sorting the right half: {right}")
   right_sort = mergesort(right,steps)
 
   return merge(left_sort,right_sort,steps) #call other function to return merged list
 
 def merge(left,right, steps):
-  steps.append{"Merging the sorted {left} and {right} arrays")
+  steps.append{"Inititating the merge between the sorted left: {left} and right: {right} arrays")
   result = [] #initialize list
   i = j = 0 #set equal to 0
 
   while i < len(left) and j < len(right): #compare elements and append the smaller
     if left[i] < right[j]:
       result.append(left[i])
-      steps.append("")
+      steps.append("Comparing {left[i]} left and {right[j]} right\n {left[i]} is smaller than; adding to result: {result}")
       i +=1 #increment i by 1
     else:
       result.append(right[j])
-      steps.append("")
+      steps.append("Comparing {right[j]} right and {left[i]} left\n {right[j]} is smaller than; adding to the result: {result}")
       j +=1 #increment j by one
 
 
+  while i < len(left):
+    result.append(left[i])
+    steps.append("Right list is empty\n Adding leftover element {left[i]} from left list to result {result}")
+    i +=1
 
-def merge_sort_steps_display(
+  while j < len(right):
+    result.append(right[j])
+    steps.append("Left list is empty \n Adding leftover element {right[i]} from right list to result {result}")
+    j+=1
+
+
+  steps.append("Merging complete. Final merged list: {result}")
+
+
+
 
 
